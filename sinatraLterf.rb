32238e20f3b21b63ae8ddf957cb367e6d2527e07
Chris Kontomitros <chkon2009@yahoo.gr>
2018-04-16 21:03:14 +0300

    Modular implementation and route list

--- sinatraLterf.rb
+++ sinatraLterf.rb
@@ -1,183 +1,204 @@
-require 'sinatra'
 require 'webrick'
-get '/bs/get' do 
-	query = params['node']
-	unless query
-		"You have to include the AP ID number in the query."
-	else
-		params.delete('node')
-		unless params.empty?
-			"HTML5/XML response to be built..."
+#require 'sinatra/base'
+require 'sinatra'
+class LteService  < Sinatra::Application
+	@@list_methods=Array.new
+
+		
+	get '/bs/get' do 
+		query = params['node']
+		kati="You have to include the AP ID number in the query."
+		unless query
+			kati
 		else
-			WEBrick::HTTPStatus::BadRequest = "Missing parameter"
-			
+			params.delete('node')
+			unless params.empty?
+				"HTML5/XML response to be built..."
+			else
+				WEBrick::HTTPStatus::BadRequest = "Missing parameter"
+				
+			end
+		end
+
+	end
+
+	get '/bs/set' do
+		query = params['node']
+		unless query
+			"You have to include the AP ID number in the query."
+		else
+			params.delete('node')
+			unless params.empty?
+				"HTML5/XML response to be built..."
+			else
+				WEBrick::HTTPStatus::BadRequest = "Missing parameter"
+				
+			end
+		end	
+
+
+	end
+
+	get '/epc/get' do
+		query = params['node']
+		unless query
+			"You have to include the AP ID number in the query."
+		else
+			params.delete('node')
+			unless params.empty?
+				"HTML5/XML response to be built..."
+			else
+				WEBrick::HTTPStatus::BadRequest = "Missing parameter"
+				
+			end
+		end	
+
+	end
+
+	get '/epc/set' do
+		query = params['node']
+		unless query
+			"You have to include the AP ID number in the query."
+		else
+			params.delete('node')
+			unless params.empty?
+				"HTML5/XML response to be built..."
+			else
+				WEBrick::HTTPStatus::BadRequest = "Missing parameter"
+				
+			end
 		end
 	end
 
-end
-
-get '/bs/set' do
-	query = params['node']
-	unless query
-		"You have to include the AP ID number in the query."
-	else
-		params.delete('node')
-		unless params.empty?
-			"HTML5/XML response to be built..."
+	get '/bs/config/save' do
+		query = params['node']
+		unless query
+			"You have to include the AP number that you want to save its configuration."
 		else
-			WEBrick::HTTPStatus::BadRequest = "Missing parameter"
-			
-		end
-	end	
-
-
-end
-
-get '/epc/get' do
-	query = params['node']
-	unless query
-		"You have to include the AP ID number in the query."
-	else
-		params.delete('node')
-		unless params.empty?
-			"HTML5/XML response to be built..."
-		else
-			WEBrick::HTTPStatus::BadRequest = "Missing parameter"
-			
-		end
-	end	
-
-end
-
-get '/epc/set' do
-	query = params['node']
-	unless query
-		"You have to include the AP ID number in the query."
-	else
-		params.delete('node')
-		unless params.empty?
-			"HTML5/XML response to be built..."
-		else
-			WEBrick::HTTPStatus::BadRequest = "Missing parameter"
-			
+			name = params['name']
+			unless name
+				"You did not include a name to save the configuration."
+			else
+				"Stuff to be implemented"
+				
+			end
 		end
 	end
-end
-
-get '/bs/config/save' do
-	query = params['node']
-	unless query
-		"You have to include the AP number that you want to save its configuration."
-	else
-		name = params['name']
-		unless name
-			"You did not include a name to save the configuration."
+	get '/bs/config/load' do
+		query = params['node']
+		unless query
+			"You have to include the AP number that you want to load a specific configuration."
 		else
-			"Stuff to be implemented"
-			
+			name = params['name']
+			unless name
+				"You did not include the name of the configuration."
+			else
+				"Stuff to be implemented"
+				
+			end
 		end
 	end
-end
-get '/bs/config/load' do
-	query = params['node']
-	unless query
-		"You have to include the AP number that you want to load a specific configuration."
-	else
+
+	get '/bs/config/convert' do
 		name = params['name']
 		unless name
 			"You did not include the name of the configuration."
 		else
 			"Stuff to be implemented"
-			
+				
 		end
+		
 	end
-end
-
-get '/bs/config/convert' do
-	name = params['name']
-	unless name
-		"You did not include the name of the configuration."
-	else
-		"Stuff to be implemented"
-			
+	get '/bs/config/list' do
+		"List elements to be implemented ..."
 	end
-	
-end
-get '/bs/config/list' do
-	"List elements to be implemented ..."
-end
-get '/bs/config/delete' do
-	name = params['name']
-	unless name
-		"You have to specify the name of the configuration to be deleted."
-	else
-		"Stuff to be implemented"
-			
-	end
-	
-end
-
-end
-
-get '/epc/config/save' do
-	name = params['name']
-	unless name
-		"You did not include a name to save the configuration."
-	else
-		"Stuff to be implemented"
-			
-	end
-	
-end
-
-get '/epc/config/list' do
-	"List elements to be implemented ..."
-end
-
-get '/epc/config/save' do
-	name = params['name']
-	unless name
-		"You did not include a name to save the configuration."
-	else
-		"Stuff to be implemented"
-			
-	end
-	
-end
-
-get '/epc/config/delete' do
-	name = params['name']
-	unless name
-		"You have to specify the name of the configuration to be deleted."
-	else
-		"Stuff to be implemented"
-			
-	end
-	
-end
-
-get '/epc/config/testbed' do
-	name = params['name']
-	unless name
-		"You have to include the name of the testbed to use by appending ?name=indoor or ?name=outdoor to your query."
-	else
-		"Stuff to be implemented"
-			
-	end
-	
-end
-
-get '/epc/config/set' do
-	query = params['node']
-	unless query
-		"You have to include the AP ID number in the query."
-	else
-		params.delete('node')
-		unless params.empty?
-			"HTML5/XML response to be built..."
+	get '/bs/config/delete' do
+		name = params['name']
+		unless name
+			"You have to specify the name of the configuration to be deleted."
 		else
-			WEBrick::HTTPStatus::BadRequest = "Missing parameter"
-			
+			"Stuff to be implemented"
+				
+		end
+		
+	end
+
+	
+
+	get '/epc/config/save' do
+		name = params['name']
+		unless name
+			"You did not include a name to save the configuration."
+		else
+			"Stuff to be implemented"
+				
+		end
+		
+	end
+
+	get '/epc/config/list' do
+		"List elements to be implemented ..."
+	end
+
+	get '/epc/config/save' do
+		name = params['name']
+		unless name
+			"You did not include a name to save the configuration."
+		else
+			"Stuff to be implemented"
+				
+		end
+		
+	end
+
+	get '/epc/config/delete' do
+		name = params['name']
+		unless name
+			"You have to specify the name of the configuration to be deleted."
+		else
+			"Stuff to be implemented"
+				
+		end
+		
+	end
+
+	get '/epc/config/testbed' do
+		name = params['name']
+		unless name
+			"You have to include the name of the testbed to use by appending ?name=indoor or ?name=outdoor to your query."
+		else
+			"Stuff to be implemented"
+				
+		end
+		
+	end
+
+	get '/epc/config/set' do
+		query = params['node']
+		unless query
+			"You have to include the AP ID number in the query."
+		else
+			params.delete('node')
+			unless params.empty?
+				"HTML5/XML response to be built..."
+			else
+				WEBrick::HTTPStatus::BadRequest = "Missing parameter"
+				
+			end
 		end
 	end
+	self.routes["GET"].each do |route|
+  		@@list_methods<<route[0]
+	end
+
+	
+
+	get '/' do
+		output=''
+		@@list_methods.each do |route|
+  		output << "#{route} <br />"
+  		end
+  		return output
+  	end
+	run! if app_file == $0
 end
